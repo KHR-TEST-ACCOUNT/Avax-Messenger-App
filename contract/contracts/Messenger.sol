c8eee84fce57f27560b44a81defa076cf507f430
koya <azumabass@gmail.com>
2023-04-14 22:39:56 +0900

    メッセージの保存機能を実装  (#3)

--- contract/contracts/Messenger.sol
+++ contract/contracts/Messenger.sol
@@ -14,10 +14,66 @@ contract Messenger {
     // 状態変数のstateを定義 → 読み取って実行を確認できる → Getter なので、State() で呼び出し
     uint256 public state;
 
-    constructor() {
+    /**
+     * @dev メッセージの送り手、受け手を格納。その他、保留中などのデータを格納
+     * 1ETH ＝ 1,000,000,000,000,000,000 wei (10^18)
+     */
+    struct Message {
+        address payable sender; // Payable -> トークンのやり取りを可能にする型
+        address payable receiver;
+        uint256 depositInWe; // メッセージトークンの量(wei → トークンの料金)を表します。
+        uint256 timestamp;
+        string text;
+        bool isPending; // 保留かどうか
+    }
+
+    // private -> コントラクトないからのみ呼び出し可能
+    // Solidityでは, アクセス修飾子がついてない変数を, 自動的にinternalとして扱います。
+    // メッセージ受取人のアドレスをKey、 その人あてのメッセージを保存するMap
+    mapping(address => Message[]) private messageAtAddress;
+
+    // payableはトランザクションにトークンのやり取りが発生することを伝える関数修飾子
+    constructor() payable {
         // コントラクトが呼び出されたときに出力される。
         console.log("Here is my first smart contract!");
+    }
 
-        state = 1;
+    /**
+     * メッセージを送信する。関数を呼び出した人のアドレスと トークン を読み取る。
+        関数にpayableを指定するとその関数の呼び出しにトークンを送ることができます。 
+        逆にpayableが無い関数にトークンを送ろうとすると関数はトランザクションを拒否します。
+        ⭐ payable → となっているので、コントラクトに支払情報、アカウントなどのデータを記憶する。
+     * @param _text  メッセージ本文
+     * @param _receiver 受け手
+     */
+    function post(
+        string memory _text,
+        address payable _receiver
+    ) public payable {
+        // コンソールに表示
+        console.log(
+            "%s -> posts text:[%s], token:[%d]",
+            msg.sender,
+            _text,
+            msg.value
+        );
+
+        // Map に格納 →
+        messageAtAddress[_receiver].push(
+            Message(
+                payable(msg.sender), // ⭐関数を呼び出した User をPayable型にキャスト
+                _receiver,
+                msg.value, // gus 代 Wai -> ⭐関数呼び出し時に送信されたトークンの値
+                block.timestamp,
+                _text,
+                true // 全てのメッセージは投稿直後は保留状態となる
+            )
+        );
+    }
+
+    // 関数を呼び出したユーザのアドレス宛のメッセージを全て取得します。
+    // view → 読み込みだけなのでガス代が　かからない。
+    function getOwnMessages() public view returns (Message[] memory) {
+        return messageAtAddress[msg.sender];
     }
 }
--- contract/test/Messenger.ts
+++ contract/test/Messenger.ts
@@ -1,22 +1,99 @@
 /** @format */
 
-import hre from 'hardhat';
+import hre, { ethers } from 'hardhat';
+import { Overrides } from 'ethers';
 import { expect } from 'chai';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 
+/**
+ * 
 // test at TS -> Mocha
 //   it関数をdescribeの引数（の関数）内に渡すことで, 個々のテストを1つのdescribeでグループ化。
 // hre.ethers.getContractFactory("Messenger")の処理が終わるまで,
 //   async function関数の中に記載されている他の処理は実行されない
-describe('Messenger', function () {
-    // テストを宣言 -> Mocha
-    it('should deploy construct', async function () {
-        const MessengerFactory = await hre.ethers.getContractFactory(
-            'Messenger'
-        );
-        const MessengerContract = await MessengerFactory.deploy();
+ */
 
-        // expectは chai ライブラリの機能
-        // state)は, 自動的にgetter関数がコンパイラにより作られます。
-        expect(await MessengerContract.state()).to.equal(1);
+/**
+ * 各テストで呼び出されるヘルパー関数
+ * Hardhat が用意するローカルether ネットワークにデプロイして、デプロイの情報を返却する。
+ * @returns デプロイの結果の、コントラクト、トークン数、アカウント
+ */
+async function deployContract() {
+    // ethers.getSigners → 第一、第2引数に etherのアカウントを返す。
+    // Hardhatが提供する任意のアドレスを配列で返す関数。 10000ether を持っている。
+    const [ownerAccount, otherAccount] = await ethers.getSigners();
+
+    // トークンの初期値。 関数を呼び出すのに、いくつかトークンを持っていることにする。
+    const funds = 100;
+
+    // コントラクトを変数に格納する用意をする
+    const MessengerFactory = await hre.ethers.getContractFactory('Messenger');
+
+    // コンストラクターの呼び出しをオーバーライド
+    // contract.METHOD_NAME(...args [ , overrides ] ) ⇒ Promise< any >とする。
+    // Value → 関数を呼び出すアカウントのValue → アカウントの初期トークン量 を指定。
+    //   → トークンは、関数を呼び出しているアカウントから引かれる。
+    const MessengerContract = await MessengerFactory.deploy({
+        value: funds,
+    } as Overrides);
+
+    // デプロイの結果の、コントラクト、トークン数、アカウントをリターン
+    return { MessengerContract, funds, ownerAccount, otherAccount };
+}
+
+describe('Post', function () {
+    // トークンが送られたかどうかのテスト
+    it('Should send the correct amount of tokens', async function () {
+        // loadFixture → 引数の関数の戻り値を格納。
+        //   → 初回の呼び出し時は、格納して初期化。　次回以降は、1回目の初期化の値を返す。
+        //   → 無駄なメモリを使わない。
+        const { MessengerContract, ownerAccount, otherAccount } =
+            await loadFixture(deployContract);
+
+        // 送るトークンを設定
+        const testDeposit = 10;
+
+        // テスト実行
+        // {value: testDeposit,} → コントラクトを呼び出すときのアカウントのValue
+        //   → ⭐アカウントの残高からいくら引いて、Payable 関数に渡して、コントラクトに保存するか
+        // expect(a).to.changeEtherBalances(b,c) → 第一と、第2、それぞれの変化をテストする。
+        //   → a.関数実行後の、 b.増減の結果 を記述する。
+        await expect(
+            MessengerContract.post('test_text_v1', otherAccount.address, {
+                value: testDeposit,
+            })
+        ).to.changeEtherBalances(
+            [ownerAccount, MessengerContract],
+            [-testDeposit, testDeposit]
+        );
+    });
+
+    // 正しいメッセージを発信することをテスト
+    it('Should set the right Message', async function () {
+        const { MessengerContract, ownerAccount, otherAccount } =
+            await loadFixture(deployContract);
+        const testDeposit = 1;
+        const testText = 'test_text_v2';
+        await MessengerContract.post(testText, otherAccount.address, {
+            value: testDeposit,
+        });
+
+        // メッセージの受け手 の アカウントで、コントラクトの関数を呼び出す。
+        const messageResults = await MessengerContract.connect(
+            otherAccount
+        ).getOwnMessages();
+
+        // Message 構造体の配列が返るので、その 0番目を取得 → 送ったメッセージ
+        const message = messageResults[0];
+
+        // Message 構造体の 中身を確認する。
+        expect(message.sender).to.equal(ownerAccount.address);
+        expect(message.receiver).to.equal(otherAccount.address);
+        expect(message.depositInWe).to.equal(testDeposit);
+        expect(message.text).to.equal(testText);
+        expect(message.isPending).to.equal(true);
+
+        // 最後に表示する
+        console.log(message);
     });
 });
