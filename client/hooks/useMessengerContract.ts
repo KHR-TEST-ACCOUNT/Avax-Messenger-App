733edf1c28da221cddfa51df8ec4987f9683e79e
koya <azumabass@gmail.com>
2023-04-18 16:54:24 +0900

    Ethereumのプロバイダーを取得(#19)

--- client/hooks/useMessengerContract.ts
+++ client/hooks/useMessengerContract.ts
@@ -4,6 +4,7 @@ import { BigNumberish, ethers } from 'ethers';
 import abi from '@/utils/Messenger.json';
 import { getEtherum } from '@/utils/ethereum';
 import { Messenger as MessengerType } from '@/typechain-types';
+import { useState } from 'react';
 
 // コントラクトアドレスを使用してコントラクトに接続する。
 const contractAddress = '0x1A39EAAfd55D97b7e499ab5f7f8055a40DcF4183';
@@ -21,8 +22,60 @@ export type Message = {
     isPending: boolean;
 };
 
+// メッセージの型
 type SendMessageProps = {
     text: string;
     reciever: string;
     tokenInEther: string;
+};
+
+// UseState で返すデータの型を定義
+type ReturnUseMessengerContract = {
+    ownMessages: Message[];
+    processing: boolean;
+    // 親で呼出して、子で処理する。
+    sendMessage: (props: SendMessageProps) => void;
+};
+
+type UseMessengerContractPsops = {
+    // アカウントは取得できるときと、できない時がある。
+    currentAccount: string | undefined;
+};
+
+/**
+ * ReturnUseMessengerContract → UseState で返す関数。
+ * 親で呼出して、子で処理する。  → currentAccount を更新する。
+ */
+export const useMessengerContract = ({
+    currentAccount,
+}: UseMessengerContractPsops): ReturnUseMessengerContract => {
+    // ユーザ宛のメッセージを配列で保持する状態変数。
+    const [ownMessages, setOwnMessages] = useState<Message[]>([]);
+
+    // トランザクションの処理中のフラグを表す状態変数。
+    const [processing, setProcessing] = useState<boolean>(false);
+
+    // Messenger コントラクトを 格納する状態変数 → 初期値なし
+    const [messengerContract, setMessengerContract] = useState<MessengerType>();
+
+    // ethereum プロバイダーを取得
+    const ethereum = getEtherum();
+
+    /** コントラクトを取得する */
+    function getMessengerContract() {
+        try {
+            if (ethereum) {
+                //  ethers.providers.Web3Provider → Ethereumのプロバイダーを取得する
+                // provider (= MetaMask) を設定しています。
+                // providerを介して,ユーザーはブロックチェーン上に存在するノードに接続することができます。
+                const provider = new ethers.providers.Web3Provider(ethereum);
+
+                // signerは,ユーザーのウォレットアドレスを抽象化したもの
+                const signer = provider.getSigner();
+            } else {
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 };
--- client/utils/ethereum.ts
+++ client/utils/ethereum.ts
@@ -1,13 +1,13 @@
 /** @format */
 
 // Metamask の拡張機能のプロバイダーと通信する
-import { MetaMaskInpageProvider } from '@metamask/providers';
+import { BaseProvider } from '@metamask/providers';
 
 // Window に ether を追加。 Ether はMeatamask の型にして取得。
 //   → 取得時に、Window オブジェクトを通じて 署名を求めたりしてくれる。
 declare global {
     interface Window {
-        ethereum?: MetaMaskInpageProvider;
+        ethereum?: BaseProvider;
     }
 }
 
@@ -25,16 +25,30 @@ declare global {
  * ユーザーが接続しているブロックチェーンからデータを読み取り,
  * ユーザーがメッセージや取引に署名するよう求めることができます。
  *
+ * https://zenn.dev/thanai/scraps/4c94c04bdc8373
+ *
  * @returns Ethereum
  */
-export const getEtherum = (): MetaMaskInpageProvider | null => {
+export const getEtherum = (): BaseProvider | null => {
     if (
         typeof window !== 'undefined' &&
         typeof window.ethereum !== 'undefined'
     ) {
         // window オブジェクトから Metamask の Ether情報 を取り出す。
+        // ether.jsのWeb3Providerに渡す際はMetaMaskInpageProviderだとエラーになる。
+        // Web3ProviderはEIP-1193準拠のものを受け取るようにできているので、BaseProviderを使用する。
+        // https://github.com/MetaMask/providers
+        // const { ethereum } = window as unknown as { ethereum: BaseProvider };
         const { ethereum } = window;
         return ethereum;
     }
+
+    /**
+     * MetaMaskInpageProviderはMetaMask独自の拡張やレガシーインターフェースを含んでいる。
+
+    BaseProviderはEthereum JavaScriptプロバイダ仕様（EIP-1193）を実装しているが、機能させるためにはサブクラスで変更する必要がある。
+    StreamProviderはそのようなサブクラスであり、状態を同期させ、二重ストリームを介してJSON-RPCメッセージをマーシャルします。
+    MetamaskInpageProviderは、EIP-1193に加えてレガシープロバイダインタフェースをサポートするためにStreamProviderをさらに拡張し、MetaMaskによってウェブページに注入されたオブジェクトをwindow.ethereumとしてインスタンス化するために使われます。
+    */
     return null;
 };
